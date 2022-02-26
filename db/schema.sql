98e1f28e6331ff59c779b1d20acea1038b6930cc
Chris <chrisbracewell35@gmail.com>
2022-02-26 14:32:25 -0500

    adding tables to database

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,25 @@
-DROP TABLE IF EXISTS departments;
+DROP DATABASE IF EXISTS employeeDB;
+CREATE DATABASE employeeDB;
+USE employeeDB;
 
 
-CREATE TABLE departments(
+CREATE TABLE department (
    id INTEGER AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE roles (
+     id INTEGER AUTO_INCREMENT PRIMARY KEY,
+     title VARCHAR(30) NOT NULL,
+     salary DECIMAL(10,2),
+    department_id INTEGER references department(id)
+);
+
+CREATE TABLE employee(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id  INTEGER references roles(id),
+    manager_id INTEGER references employee(id) on Delete set NULL
+);
+
